0fc4818aa6b8d6c8c944cd7f1160b5be44eb2070
Derrick Gilland <dgilland@gmail.com>
2015-05-07 23:13:50 -0400

    Add project metadata.

---
+++ verify/__meta__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Define project metadata
+"""
+
+__title__ = 'verify'
+__summary__ = 'A painless assertion library for Python.'
+__url__ = 'https://github.com/dgilland/verify'
+
+__version__ = '0.0.1'
+
+__install_requires__ = []
+
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+
+__license__ = 'MIT License'
